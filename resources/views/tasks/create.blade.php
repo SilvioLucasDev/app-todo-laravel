44b239f875c9421fbd6a0d1176a6fa1ec3e21d20
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 12:59:29 -0300

    feat: add view tasks create

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,9 @@
+<x-layout page="Criar">
+    <x-slot:btn>
+        <a href="{{ route('home.index') }}" class="btn btn-primary">
+            Voltar
+        </a>
+    </x-slot:btn>
+
+    Tela de criar uma tarefa.
+</x-layout>
