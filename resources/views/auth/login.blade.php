b5d0dfe3d60a903544d504ef834e4c500db7957c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 19:53:46 -0300

    feat: add form to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,42 @@
 <x-layout page="Login">
     <x-slot:btn>
-        <a href="{{ route('register') }}" class="btn btn-primary">
+        <a href="{{ route('auth.register') }}" class="btn btn-primary">
             Registre-se
         </a>
     </x-slot:btn>
 
-    Tela de Login &nbsp;
-    <a href="{{ route('home.index') }}">
-        Ir para a Home
-    </a>
+    <section class="task_section">
+        <h1>Login</h1>
+
+        {{-- @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif --}}
+
+        <form action="{{ route('auth.login_action') }}" method="POST">
+            @csrf
+            <x-form.text-input name="email" label="E-mail" type="email" placeholder="Digite o seu e-mail"
+                required="required" value="{{ old('email') }}">
+            </x-form.text-input>
+            {{ $errors->has('email') ? $errors->first('email') : '' }}
+
+            <x-form.text-input name="password" label="Senha" type="password" placeholder="Digite a senha"
+                required="required" value="{{ old('password') }}">
+            </x-form.text-input>
+            {{ $errors->has('password') ? $errors->first('password') : '' }}
+
+            <x-form.form-button type="reset">
+                <x-form.button type="reset">
+                    Limpar
+                </x-form.button>
+
+                <x-form.button type="submit">
+                    Entrar
+                </x-form.button>
+            </x-form.form-button>
+        </form>
+    </section>
 </x-layout>
