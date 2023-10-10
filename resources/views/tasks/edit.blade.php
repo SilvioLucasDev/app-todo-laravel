0eaa4741aebe518f053060cf6cc252b2a3e6b2cd
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 12:59:22 -0300

    feat: add view tasks edit

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,9 @@
+<x-layout page="Editar">
+    <x-slot:btn>
+        <a href="{{ route('home.index') }}" class="btn btn-primary">
+            Voltar
+        </a>
+    </x-slot:btn>
+
+    Tela de editar uma tarefa.
+</x-layout>
