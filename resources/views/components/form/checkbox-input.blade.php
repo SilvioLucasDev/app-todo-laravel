5f0a249f5dcb55cdf278c509d591d1e43a5eb402
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 16:42:31 -0300

    feat: add checkbox input component

---
+++ resources/views/components/form/checkbox-input.blade.php
@@ -0,0 +1,7 @@
+<div class="input-area">
+    <label for="{{ $name }}">
+        {{ $label ?? '' }}
+    </label>
+    <input id="{{ $name }}" name="{{ $name }}" type="checkbox"
+        {{ empty($required) ? '' : 'required' }}value="1" {{ empty($checked) ? '' : 'checked' }}>
+</div>
