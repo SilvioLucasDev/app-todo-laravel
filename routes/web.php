1d22f17a47353c2a653402e8e71e759a29b2a864
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 13:00:56 -0300

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+
+// Route::get('/tasks', [TaskController::class, 'index'])->name('task.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('task.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('task.store');
+Route::get('/tasks/{id}', [TaskController::class, 'create'])->name('task.show');
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('task.edit');
+Route::match(['put', 'patch'], '/tasks/{id}', [TaskController::class, 'update'])->name('task.update');
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
