5f97dbffa525b14acca7b9e479f30db9d6cae115
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-10 14:55:42 -0300

    feat: add form textarea input component

---
+++ resources/views/components/form/textarea-input.blade.php
@@ -0,0 +1,8 @@
+<div class="input-area">
+    <label for="{{ $name }}">
+        {{ $label ?? '' }}
+    </label>
+
+    <textarea id="{{ $name }}" name="{{ $name }}" placeholder="{{ $placeholder ?? '' }}"
+        {{ empty($required) ? '' : 'required' }}></textarea>
+</div>
